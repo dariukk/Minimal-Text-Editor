4d751e19c2cba646f6d94e1245b7cb52f3f8ad38
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-03-28 06:28:17 -0700

    Add files via upload

---
+++ editor.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// declararea atat a listei pentru text
+// cat si a copiei acesteia
+typedef struct Node
+{
+    char elem;
+    int line;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+typedef struct ListText
+{
+    Node *head;
+    Node *tail;
+    Node *cursor;
+} ListText;
+
+// declararea stivei pentru comenzi
+typedef struct Stack
+{
+    char *command;
+    struct Stack *next;
+
+} Stack;
+
+void init();
+
+void printList(ListText *list);
+
+void insertCharacter(char elem);
+
+int addText();
+
+void save();
+
+int doCommands();
