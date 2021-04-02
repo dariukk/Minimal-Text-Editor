ac013d33c521db496f65b6074b2589c1fc933de7
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-04-02 23:04:40 +0300

    Add files via upload

---
+++ redo.c
@@ -0,0 +1,21 @@
+
+#include "editor.h"
+
+void redo(ListText *list, Stack *undoStack, Stack *redoStack)
+{
+    NodeStack *node = pop(redoStack);
+
+    if (strcmp(node->command, addtext) == 0)
+    {
+
+        Node *p = node->list->tail;
+        while (p)
+        {
+            insertCharacter(list, p->elem);
+            p = p->prev;
+        }
+    }
+
+    free(node->command);
+    free(node);
+}
