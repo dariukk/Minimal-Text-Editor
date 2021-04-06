db9ad9bf3faf3dc48f22b4dcbaab75bb0e5c32c9
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-04-06 23:49:51 +0300

    Update undo.c

--- undo.c
+++ undo.c
@@ -3,17 +3,26 @@
 void undo(ListText *list, Stack *undoStack, Stack *redoStack)
 {
     element value = top(undoStack);
-
     pop(undoStack);
 
     if (strcmp(value.command, addtext) == 0)
     {
-
         int num = value.num;
         while (num)
         {
             insertCharacter(value.list, list->cursor->elem);
-            backspace(list);
+            backspace(list, 1);
+            --num;
+        }
+    }
+    else if (strcmp(value.command, del) == 0)
+    {
+        int num = value.num;
+        Node *node = value.list->head;
+        while (num)
+        {
+            insertCharacter(list, node->elem);
+            node = node->next;
             --num;
         }
     }
