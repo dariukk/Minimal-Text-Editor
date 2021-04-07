8e467834d4a311ba849e13dca847fcd9177e8764
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-04-07 20:59:34 +0300

    Update redo.c

--- redo.c
+++ redo.c
@@ -23,19 +23,13 @@ void redo(ListText *list, Stack *undoStack, Stack *redoStack)
             --num;
         }
     }
-    else if (strcmp(value.command, gochar) == 0)
+    else if (strcmp(value.command, gochar) == 0 || strcmp(value.command, goline) == 0)
     {
         int auxLine = list->cursor->line, auxPos = list->cursor->pos;
         gotoChar(list, value.prevPos, value.prevLine, &value, 1);
 
         value.prevLine = auxLine;
         value.prevPos = auxPos;
-    }
-    else if (strcmp(value.command, goline) == 0)
-    {
-        int auxLine = list->cursor->line;
-        gotoLine(list, value.prevLine, value, 1);
-        value.prevLine = auxLine;
     }
 
     push(undoStack, value);
