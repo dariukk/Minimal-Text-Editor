c4c10a1a2836e960005d8c6cb7677baaf7a54db2
Daria-Mihaela Florea <73852559+dariukk@users.noreply.github.com>
2021-04-08 12:30:41 +0300

    Update redo.c

--- redo.c
+++ redo.c
@@ -1,16 +1,16 @@
 #include "editor.h"
 
-void redo(ListText *list, Stack *undoStack, Stack *redoStack)
+void redo(ListText **list, Stack **undoStack, Stack **redoStack)
 {
-    element value = top(redoStack);
-    pop(redoStack);
+    element value = top(*redoStack);
+    pop(*redoStack);
 
     if (strcmp(value.command, addtext) == 0)
     {
         Node *p = value.list->tail;
         while (p)
         {
-            insertCharacter(list, p->elem);
+            insertCharacter(*list, p->elem);
             p = p->prev;
         }
     }
@@ -19,18 +19,46 @@ void redo(ListText *list, Stack *undoStack, Stack *redoStack)
         int num = value.num;
         while (num)
         {
-            backspace(list, 1);
+            backspace(*list, 1);
             --num;
         }
     }
     else if (strcmp(value.command, gochar) == 0 || strcmp(value.command, goline) == 0)
     {
-        int auxLine = list->cursor->line, auxPos = list->cursor->pos;
-        gotoChar(list, value.prevPos, value.prevLine, &value, 1);
+        int auxLine = (*list)->cursor->line, auxPos = (*list)->cursor->pos;
+        gotoChar(*list, value.prevPos, value.prevLine, &value, 1);
 
         value.prevLine = auxLine;
         value.prevPos = auxPos;
     }
+    else if (strcmp(value.command, deleteword) == 0)
+    {
+        gotoChar(*list, value.prevPos, value.prevLine, &value, 1);
 
-    push(undoStack, value);
+        char *word = (char *)malloc(sizeof(char) * 20);
+        int i = 0;
+        Node *node = value.list->head;
+        while (node)
+        {
+            word[i++] = node->elem;
+            node = node->next;
+        }
+
+        word[i] = '\0';
+        deleteWord(*list, word, &value);
+        free(word);
+    }
+    else if (strcmp(value.command, simple_replace) == 0)
+    {
+        char *aux = (char *)malloc(50 * sizeof(char));
+
+        replace(*list, value.old, value.new);
+        strcpy(aux, value.new);
+        strcpy(value.new, value.old);
+        strcpy(value.old, aux);
+
+        free(aux);
+    }
+
+    push(*undoStack, value);
 }
